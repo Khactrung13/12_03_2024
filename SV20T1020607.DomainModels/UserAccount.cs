1be57282050b7dbe1c4524001ebe468e5cd43752
Khactrung13 <97725015+Khactrung13@users.noreply.github.com>
2024-03-12 01:26:00 +0700

    abc

---
+++ SV20T1020607.DomainModels/UserAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SV20T1020607.DomainModels
+{
+	public class UserAccount
+	{
+		public string UserID { get; set; } = "";
+        public string UserName { get; set; } = "";
+        public string FullName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Photo { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string RoleNames { get; set; } = "";
+    }
+}
+
